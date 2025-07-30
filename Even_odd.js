f36244cb54e6e93fe6e4de5f743f5420c0db4d48
kalsoom irshad <daughterofirshad891@gmail.com>
2025-07-30 18:53:26 +0500

    push

--- Even_odd.js
+++ Even_odd.js
@@ -1,4 +1,10 @@
 let arr=[12,34,98,56,34]
-for(i=0;i<=arr.length;i++){
-    if()
+for(i=0;i<arr.length;i++){
+    if(arr[i]%2===0){
+        console.log("the even number in the array are" + arr[i]);
+    }
+    else {
+        console.log("the odd number in the array are" + arr[i]);
+    }
+
 }
