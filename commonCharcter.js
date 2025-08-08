a0460f1ccce425494f7d91ff12ae5f60c00414d8
kalsoom irshad <daughterofirshad891@gmail.com>
2025-08-08 18:37:34 +0500

    commonCharacter

---
+++ commonCharcter.js
@@ -0,0 +1,56 @@
+// let str1 = "apple";
+// let str2 = "plane";
+// let common = "";
+
+
+// for (let i = 0; i < str1.length; i++) {
+    
+//     for (let j = 0; j < str2.length; j++) {
+//         if (str1[i] === str2[j]) {
+//             common += str1[i];
+//             str2[j] = " ";
+//         }
+//     }
+// }
+
+ 
+
+// for(i=0;i<common.length;i++){
+//     var duplicate = false;
+
+//     for(j=i+1;j<common.length;j++){
+//         if(common[i]==common[j]){
+//            duplicate = true;
+//         }
+//     }
+//     if(duplicate){
+//         (common[i])
+//         common[i]==" ";
+//     }
+// }
+// console.log("Common characters:", common);
+let str1 = "apple";
+let str2 = "plane";
+let common = "";
+
+
+for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+        if (str1[i] === str2[j]) {
+            
+            let already = false;
+            for (let k = 0; k < common.length; k++) {
+                if (common[k] === str1[i]) {
+                    already = true;
+                    break;
+                }
+            }
+            if (!already) {
+                common += str1[i];
+            }
+            break;
+        }
+    }
+}
+
+console.log("Common characters:", common);
