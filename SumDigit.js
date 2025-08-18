b294612e8b97c80c8ac8378fa7998faa3510561d
kalsoom irshad <daughterofirshad891@gmail.com>
2025-08-18 18:04:36 +0500

    pyramid

--- SumDigit.js
+++ SumDigit.js
@@ -1,1 +1,21 @@
 
+<<<<<<< Updated upstream
+=======
+function sumUntilSingleDigit(num) {
+    while (num >= 10) {    
+        let sum = 0;
+
+       
+        while (num > 0) {
+            sum += num % 10;         
+            num = Math.floor(num/10); 
+        }
+
+        num = sum;  
+    }
+    return num;
+}
+
+console.log(sumUntilSingleDigit(9875)); 
+
+>>>>>>> Stashed changes
